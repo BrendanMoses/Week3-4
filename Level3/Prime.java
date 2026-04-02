9925b9161e62a24bf2f094dc7135dc5f52bff32e
BrendanMoses <brendanmosesjohn@gmail.com>
2026-04-02 07:09:26 +0530

    [Brendan] added : Created A Java Program to Check Prime

---
+++ Level3/Prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Prime{
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = p.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Number");
+        }
+    }
+}
